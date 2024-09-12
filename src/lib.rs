5c25efd26b1cd7eda8bfa8a4881af2348c47d432
VastSea0 <vastseaoffical0@outlook.com>
2024-09-12 22:36:05 +0300

    add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+pub enum Command {
+    Add,
+    Remove,
+    List,
+}
+
+#[derive(Debug)]
+pub struct Book {
+    pub name: String,
+    pub author: String,
+    pub date: String,
+    pub id: i32
+}
+
+fn main() {}
+
